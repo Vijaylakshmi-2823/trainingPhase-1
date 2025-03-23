b6f702bc694b1a656a11d62768c83a4501987a28
vijaylakshmi-2823 <vijaylakshmibadiger28@gmail.com>
2025-03-23 13:00:11 +0530

    code

---
+++ MultiThreading/Program2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultithreadingExample
+{
+    public class Program2
+    {
+
+
+        // Task -- we use it to manage asynchronous operations
+        static async Task PrintNumbersAsync()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 1; i <= 5; i++)
+                {
+                    Console.WriteLine($"Number {i} from Task");
+                    Task.Delay(1000).Wait();
+                }
+
+
+            });
+
+        }
+
+        static async Task Main()
+        {
+            Console.WriteLine("Started ..");
+            await PrintNumbersAsync();
+            Console.WriteLine("Completed ...");
+
+
+            // Executes tasks in parallel for faster processing
+            Parallel.For(1, 6, i =>
+
+            {
+                Console.WriteLine($"Processing {i} on thread {Task.CurrentId}");
+            }
+            );
+
+
+
+        }
+    }
+}
---
+++ MultiThreading/Restaurant.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultithreadingExample
+{
+    // Without multithreading
+    public class Restaurant
+
+    {
+
+        static async Task ProcessOrder(string customerName, int timeToPrepare)
+        {
+            Console.WriteLine($"{customerName} placed an Order");
+            await Task.Delay(timeToPrepare * 1000); //simulation for  preparation time
+            Console.WriteLine($"{customerName} order is ready {timeToPrepare}");
+
+
+        }
+
+        // here main() is an async one which allows asynchronous execution 
+        static async Task Main()
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            /*  ProcessOrder("Niti", 3);
+              ProcessOrder("Jatin", 4);
+              ProcessOrder("Aditi", 2);
+
+              sw.Stop();*/
+            // will run asynchronously in parallel  // multi 8 core cpu
+            Task t1 = ProcessOrder("Niti", 3);
+            Task t2 = ProcessOrder("Jatin", 4);
+            Task t3 = ProcessOrder("Aditi", 2);
+
+            await Task.WhenAll(t1, t2, t3); // wait for all the orders to complete
+            sw.Stop();
+
+            Console.WriteLine($"Total time taken: {sw.Elapsed.Seconds} sec");
+        }
+    }
+}
